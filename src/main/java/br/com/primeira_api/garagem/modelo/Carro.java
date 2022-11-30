efe320a08cd8c310582fa297b1edff5a9fb6f403
Rafael Pavanati <rafapavanati@gmail.com>
2022-11-29 21:17:40 -0300

    ajustes

--- src/main/java/br/com/primeira_api/garagem/modelo/Carro.java
+++ src/main/java/br/com/primeira_api/garagem/modelo/Carro.java
@@ -12,114 +12,41 @@ import javax.persistence.Table;
 
 import br.com.primeira_api.garagem.modelo.enums.Marcas;
 import br.com.primeira_api.garagem.modelo.enums.StatusSituacao;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name="carros")
+@Builder
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Carro {
 	
 	@Id 
 	@GeneratedValue(strategy = GenerationType.SEQUENCE)
 	private Long id;
+
 	private String modelo;
+
 	private String cor;
+
 	private Integer ano;
-	private BigDecimal valorCompra = new BigDecimal(2);
-	private BigDecimal valorVenda = new BigDecimal(2);
-	private BigDecimal lucro = new BigDecimal(2);
+
+	private BigDecimal valorCompra;
+
+	private BigDecimal valorVenda;
+
+	private BigDecimal lucro;
 	
 	@Enumerated(EnumType.STRING)
 	private Marcas marca;
-	
-	
-	
-	public Carro() {
-		
-	}
-
-	public Carro(String modelo, String cor, Integer ano, BigDecimal valorCompra, BigDecimal valorVenda, Marcas marca,
-			StatusSituacao situacao) {
-		this.modelo = modelo;
-		this.cor = cor;
-		this.ano = ano;
-		this.valorCompra = valorCompra;
-		this.valorVenda = valorVenda;
-		this.marca = marca;
-		this.situacao = situacao;
-	}
 
 	@Enumerated(EnumType.STRING)
 	private StatusSituacao situacao;
 
-	public Long getId() {
-		return id;
-	}
-	
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getModelo() {
-		return modelo;
-	}
-
-	public void setModelo(String modelo) {
-		this.modelo = modelo;
-	}
-
-	public String getCor() {
-		return cor;
-	}
-
-	public void setCor(String cor) {
-		this.cor = cor;
-	}
-
-	public Integer getAno() {
-		return ano;
-	}
-
-	public void setAno(Integer ano) {
-		this.ano = ano;
-	}
-
-	public BigDecimal getValorCompra() {
-		return valorCompra;
-	}
-
-	public void setValorCompra(BigDecimal valorCompra) {
-		this.valorCompra = valorCompra;
-	}
-
-	public BigDecimal getValorVenda() {
-		return valorVenda;
-	}
-
-	public void setValorVenda(BigDecimal valorVenda) {
-		this.valorVenda = valorVenda;
-	}
-
-	public Marcas getMarca() {
-		return marca;
-	}
-
-	public void setMarca(Marcas marca) {
-		this.marca = marca;
-	}
-
-	public StatusSituacao getSituacao() {
-		return situacao;
-	}
-
-	public void setSituacao(StatusSituacao situacao) {
-		this.situacao = situacao;
-	}
-	
-	public void setLucro(BigDecimal lucro) {
-		this.lucro = lucro;
-	}
-	
-	public BigDecimal getLucro() {
-		return lucro;
-	}
-	
 }
